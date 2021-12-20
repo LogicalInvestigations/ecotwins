c32b50e11d1768cf5a3a968a07d53d463a075006
Niklas Engsner <niklas.engsner@gmail.com>
2021-12-20 23:04:36 +0100

    Added all notebooks.

--- ecotwins/_nbdev.py
+++ ecotwins/_nbdev.py
@@ -2,9 +2,35 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"IN_MAIN": "animal_classes.ipynb",
+         "generate_objects": "animal_classes.ipynb",
+         "Terrain": "animal_classes.ipynb",
+         "Organism": "animal_classes.ipynb",
+         "Animal": "animal_classes.ipynb",
+         "simple_sheep_radii": "animal_classes.ipynb",
+         "simple_sheep_def": "animal_classes.ipynb",
+         "SimpleSheep": "animal_classes.ipynb",
+         "multi_sheep_radii": "animal_classes.ipynb",
+         "multi_sheep_def": "animal_classes.ipynb",
+         "MultiSheep": "animal_classes.ipynb",
+         "Ecosystem": "animal_classes.ipynb",
+         "AnimationHelper": "animation_helper.ipynb",
+         "EcoEnv": "ecoenv.ipynb",
+         "RADIUS": "perception.ipynb",
+         "ORIGIN": "perception.ipynb",
+         "Perception": "perception.ipynb",
+         "ReflexAgent": "reflex_agent.ipynb",
+         "distance": "utility.ipynb",
+         "normalize": "utility.ipynb",
+         "draw_objects": "utility.ipynb",
+         "motion_diagram": "utility.ipynb"}
 
-modules = []
+modules = ["animal_classes.py",
+           "animation_helper.py",
+           "ecoenv.py",
+           "perception.py",
+           "reflex_agent.py",
+           "utility.py"]
 
 doc_url = "https://LogicalInvestigations.github.io/ecotwins/"
 
